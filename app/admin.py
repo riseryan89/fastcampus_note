be653cc1513227de971116d2139227544d5bbd37
riseryan89 <rise.ryan.lee@gmail.com>
2023-01-27 00:43:21 +0900

    Ch 03 / Clip 03

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from app.models import Category
+from app import models as m
 
 # Register your models here.
-admin.site.register(Category)
+admin.site.register(m.Category)
+admin.site.register(m.Note)
+admin.site.register(m.UserDetail)
