67ef0c0ecba42222af525a41e9d957a7d66dc2e4
fast-campus-ryan <123443225+fast-campus-ryan@users.noreply.github.com>
2023-01-26 19:48:28 +0900

    Ch 04 / Clip 01

--- app/views/index.py
+++ app/views/index.py
@@ -1,5 +1,16 @@
+from datetime import datetime
+
 from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html', {})
+    rendering_info = dict(
+        title="Home",
+        user_id=1,
+        date_joined=datetime(2022, 1, 1, 0, 0, 0),
+        category_list=[1, 2, 3],
+        price=1000,
+        note=None,
+        msg="Hello World",
+    )
+    return render(request, "index.html", rendering_info)
