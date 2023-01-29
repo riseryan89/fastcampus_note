b05c82afbefd791ef6d4df27029f47b19fce5278
riseryan89 <rise.ryan.lee@gmail.com>
2023-01-29 19:21:49 +0900

    Ch 05 Clip 04

--- fastcampus_note/settings/gmail.py
+++ fastcampus_note/settings/gmail.py
@@ -10,7 +10,6 @@ ENV_LOAD = load_dotenv(ENV_LOC)
 if ENV_LOAD:
     config = dotenv_values(ENV_LOC)
     GMAIL_PW = config.get("GMAIL_PW")
-    print(GMAIL_PW)
 else:
     GMAIL_PW = os.environ.get("GMAIL_PW")
 
@@ -18,5 +17,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "rise.ryan.lee@gmail.com"
+EMAIL_HOST_USER = "abc@gmail.com"
 EMAIL_HOST_PASSWORD = GMAIL_PW
